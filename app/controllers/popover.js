09906bfcb4bbb8609b38fad8b2499f1ef21f6039
Cyril Fluck <github@fluck.fr>
2016-01-23 12:25:46 -0800

    wip popover

---
+++ app/controllers/popover.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import CustomPopoverContenView from '../views/custom-popover-content';
+
+export default Ember.Controller.extend({
+  contentViewClass: CustomPopoverContenView
+});
