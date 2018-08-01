b02de5464aa30443b2b973d257c31a2a9d4c8b2b
Kyle Bishop <kybishop@gmail.com>
2018-08-01 15:33:49 -0700

    make sure {{render-popover}} is rendered in a template

--- addon/components/render-popover.js
+++ addon/components/render-popover.js
@@ -1,5 +1,11 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Component.extend({
   popover: Ember.inject.service(),
+
+  init() {
+    this._super(...arguments);
+
+    this.get("popover").registerPopoverRendered();
+  }
 });
--- addon/services/popover.js
+++ addon/services/popover.js
@@ -1,11 +1,19 @@
-import Ember from 'ember';
+import Ember from "ember";
 
 export default Ember.Service.extend({
   init() {
     this._super(...arguments);
     this.popovers = Ember.A([]);
   },
+
   open(destinationElement, ComponentClass, options) {
+    if (!this._popoverDidRender) {
+      throw new Error(
+        "{{render-popover}} must be included in a template for popovers to open. " +
+          "Unit-integration tests don't include application.hbs; you must add {{render-popover}} to this.render()."
+      );
+    }
+
     let popoverSpec = {
       destinationElement,
       ComponentClass,
@@ -14,12 +22,16 @@ export default Ember.Service.extend({
 
     let closePopover = () => {
       this.popovers.removeObject(popoverSpec);
-    }
+    };
 
     popoverSpec.options.closePopover = closePopover;
 
     this.popovers.pushObject(popoverSpec);
 
     return closePopover;
+  },
+
+  registerPopoverRendered() {
+    this._popoverDidRender = true;
   }
 });
