2fffa0a91e53990c7e79f4653af6c9fadcc4d016
Cory Forsyth <cory.forsyth.temp@addepar.com>
2018-09-13 15:11:59 -0400

    Ensure classNames prop is concatenated, not stomped

--- addon/mixins/new-popover-modal-api.js
+++ addon/mixins/new-popover-modal-api.js
@@ -1,4 +1,5 @@
-let BINDING_RE = /Binding$/;
+const BINDING_RE = /Binding$/;
+const CONCAT_PROPS = ['classNames'];
 
 export default Ember.Mixin.create({
   init(...args) {
@@ -11,9 +12,12 @@ export default Ember.Mixin.create({
   _setupProperties(props={}) {
     let boundPropKeys = [];
     let bareProps = {};
+    let concatPropKeys = [];
     Object.keys(props).forEach(key => {
       if (BINDING_RE.test(key)) {
         boundPropKeys.push(key);
+      } else if (CONCAT_PROPS.includes(key)) {
+        concatPropKeys.push(key);
       } else {
         bareProps[key] = props[key];
       }
@@ -27,5 +31,11 @@ export default Ember.Mixin.create({
 
     // set non-bound props
     this.setProperties(bareProps);
+
+    for (let key of concatPropKeys) {
+      let originalProps = this.get(key) || [];
+      let newProps = props[key] || [];
+      this.set(key, [...originalProps, ...newProps]);
+    }
   }
 });
