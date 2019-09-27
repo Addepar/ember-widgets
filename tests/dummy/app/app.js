42e0c1095f72c35e09d34c8bd259c0d54cc90697
Cyril Fluck <github@fluck.fr>
2019-09-27 11:17:00 -0700

    PR comment

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import CarouselItem from 'ember-widgets/views/carousel-item';
 
 var App;
 
@@ -13,9 +12,6 @@ App = Ember.Application.extend({
   podModulePrefix: config.podModulePrefix,
   Resolver: Resolver
 });
-
-// Legacy view-as-component weirdness. Don't ever ever ever do this.
-Ember.Helper.helper('carousel-item', CarouselItem);
 
 loadInitializers(App, config.modulePrefix);
 
