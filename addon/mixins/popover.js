893373bf7ea0541198f7d1a7dca4eb02c79e3736
Cyril Fluck <github@fluck.fr>
2019-10-10 17:00:33 -0700

    Another fix?

--- addon/mixins/popover.js
+++ addon/mixins/popover.js
@@ -42,7 +42,7 @@ export default Ember.Mixin.create(StyleBindingsMixin, BodyEventListener, {
   }).property('contentViewClass'),
   didInsertElement: function() {
     this._super();
-    Ember.run.schedule('afterRender', this, () => {
+    Ember.run.schedule('actions', this, () => {
       this.snapToPosition();
       this.set('visibility', 'visible');
       this.set('isShowing', true);
